7bc0aa61e6138a1b7ba1d2a9ccf9c762ebd005c5
Clemens Hasler <clemens@synopsys.com>
2021-11-02 10:19:10 +0100

    Improve test hand

--- test_player.py
+++ test_player.py
@@ -56,7 +56,7 @@ class PlayerTest(unittest.TestCase):
                     "hole_cards": [                         # The cards of the player. This is only visible for your own player
                                                             #     except after showdown, when cards revealed are also included.
                         {
-                            "rank": "6",                    # Rank of the card. Possible values are numbers 2-10 and J,Q,K,A
+                            "rank": "K",                    # Rank of the card. Possible values are numbers 2-10 and J,Q,K,A
                             "suit": "hearts"                # Suit of the card. Possible values are: clubs,spades,hearts,diamonds
                         },
                         {
